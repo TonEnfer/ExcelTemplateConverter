001f6e0f8c29cb9e4033fd5660105724c75bc5f8
TonEnfer <tonik.sam@gmail.com>
2018-05-16 14:48:46 +0500

    First commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml;
+using System.Diagnostics;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        const string path = "E:\\";
+        const string fileName2 = "по счету 101.00 за 15 марта 2018 г_.xlsx";
+        const string fileName3 = "по счету 29 за 15 марта 2018 г_.xlsx";
+        const string fileName = "test.xlsx";
+
+
         static void Main(string[] args)
         {
+            Report newReport = new Report(Path.Combine(path, fileName)),
+                oldReport = new Report(Path.Combine(path, fileName2)),
+                oldReport2 = new Report(Path.Combine(path, fileName3));
+
+            newReport.CreateExcelDoc();
+            newReport.WriteExcelDoc();
+            List<Dataset> datasets1 = oldReport.ReadExcelDoc();
+            List<Dataset> datasets2 = oldReport2.ReadExcelDoc();
+
+            Console.ReadKey();
         }
     }
 }
